c033f2996835529ee99862df3aa8d3785f7a6579
Rupertofly <Rupertofly@users.noreply.github.com>
2018-04-11 16:33:59 +1000

    push to test

--- sketch.js
+++ sketch.js
@@ -36,15 +36,61 @@ function recordSetup () {
 // #endregion
 function preload () {}
 let canvas;
+let database;
+let promptS, question, buTrust, buBetray, Qno;
 function setup () {
-  canvas = createCanvas(300, 500);
+  var config = {
+    apiKey: 'AIzaSyCZh7bDhcHYesPc0FeKxriL7EZ2Kopk2us',
+    authDomain: 'awesomesaucerupert.firebaseapp.com',
+    databaseURL: 'https://awesomesaucerupert.firebaseio.com',
+    projectId: 'awesomesaucerupert',
+    storageBucket: 'awesomesaucerupert.appspot.com',
+    messagingSenderId: '465094389233'
+  };
+  firebase.initializeApp(config);
+  Qno = 0;
+  noCanvas();
   // recordSetup();
+  database = firebase.database();
+
+  let cat = database.ref('inputData');
+  promptS = '';
+  question = createDiv('');
+  buTrust = createButton('Trust');
+  buBetray = createButton('Betray');
+  buTrust.mousePressed(function () {
+    pushData('Trust');
+  });
+  buBetray.mousePressed(function () {
+    pushData('Betray');
+  });
+
+  newQuestion();
 }
 function draw () {
   background(getC(hues.neutrals, 1).hex);
   if (mouseIsPressed) {
     ellipse(mouseX, mouseY, 30, 30);
   }
-
+  // promptS = promptS + 1;
   // recordFrame();
+}
+let questionData;
+let options = ['green', 'pink', 'yellow', 'murder'];
+
+function newQuestion () {
+  Qno++;
+  let thisPrompt = options[Math.floor(Math.random() * 4)];
+  question.html("quick it's " + thisPrompt);
+  questionData = {
+    qNo: Qno,
+    prompt: thisPrompt,
+    answer: null
+  };
+}
+function pushData (option) {
+  questionData.answer = option;
+  let cat = database.ref('inputData');
+  cat.push(questionData);
+  newQuestion();
 }
